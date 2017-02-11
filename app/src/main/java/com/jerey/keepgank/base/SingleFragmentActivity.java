67c3f71546ead08a29722e0f0aec653fdfb07bc6
Xiamin <610315802@qq.com>
2017-02-11 14:04:53 +0800

    增加滑动返回库，并设计基础类，滑动返回的Fragment

---
+++ app/src/main/java/com/jerey/keepgank/base/SingleFragmentActivity.java
@@ -0,0 +1,30 @@
+package com.jerey.keepgank.base;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.jerey.keepgank.R;
+
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
+
+/**
+ * Created by Xiamin on 2017/2/11.
+ */
+
+public abstract class SingleFragmentActivity extends SwipeBackActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+        Fragment fragment = getFragment();
+        fragment.setArguments(getArguments());
+        getSupportFragmentManager().beginTransaction().add(android.R.id.content, fragment).commit();
+
+    }
+
+
+    protected abstract Fragment getFragment();
+
+    protected abstract Bundle getArguments() ;
+}
