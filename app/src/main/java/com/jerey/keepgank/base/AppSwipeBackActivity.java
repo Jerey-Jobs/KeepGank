520600cbd33fedcef8a0587c0d6260233e9f8fd9
Xiamin <610315802@qq.com>
2017-02-25 21:54:17 +0800

    实现AppSwipeBackActivity extends AppCompatActivity而非第三方库的继承与Fragment

---
+++ app/src/main/java/com/jerey/keepgank/base/AppSwipeBackActivity.java
@@ -0,0 +1,59 @@
+package com.jerey.keepgank.base;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.Utils;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityBase;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityHelper;
+
+/**
+ * Created by Xiamin on 2017/2/25.
+ */
+
+/**
+ * Caused by: java.lang.ClassCastException: com.jerey.keepgank.activity.WebActivity
+ * cannot be cast to android.support.v7.app.AppCompatActivity
+ */
+public class AppSwipeBackActivity extends AppCompatActivity implements SwipeBackActivityBase {
+    private SwipeBackActivityHelper mHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mHelper = new SwipeBackActivityHelper(this);
+        mHelper.onActivityCreate();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mHelper.onPostCreate();
+    }
+
+    @Override
+    public View findViewById(int id) {
+        View v = super.findViewById(id);
+        if (v == null && mHelper != null)
+            return mHelper.findViewById(id);
+        return v;
+    }
+
+    @Override
+    public SwipeBackLayout getSwipeBackLayout() {
+        return mHelper.getSwipeBackLayout();
+    }
+
+    @Override
+    public void setSwipeBackEnable(boolean enable) {
+        getSwipeBackLayout().setEnableGesture(enable);
+    }
+
+    @Override
+    public void scrollToFinishActivity() {
+        Utils.convertActivityToTranslucent(this);
+        getSwipeBackLayout().scrollToFinishActivity();
+    }
+}
