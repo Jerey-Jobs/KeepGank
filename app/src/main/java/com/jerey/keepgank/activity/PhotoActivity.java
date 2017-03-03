e577f5027f63aaa497976c65580e4763934077cb
Jerey-Jobs <610315802@qq.com>
2017-03-03 17:53:16 +0800

    增加图片保存功能

--- app/src/main/java/com/jerey/keepgank/activity/PhotoActivity.java
+++ app/src/main/java/com/jerey/keepgank/activity/PhotoActivity.java
@@ -3,15 +3,22 @@ package com.jerey.keepgank.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.data.StreamAssetPathFetcher;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.jerey.keepgank.R;
 import com.jerey.keepgank.View.PinchImageView;
 import com.jerey.keepgank.base.AppSwipeBackActivity;
+import com.jerey.keepgank.utils.ImageSaveUtil;
+import com.orhanobut.logger.Logger;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -20,20 +27,25 @@ import butterknife.ButterKnife;
  * Created by Xiamin on 2017/3/2.
  */
 
-public class PhotoActivity extends AppSwipeBackActivity{
+public class PhotoActivity extends AppSwipeBackActivity implements View.OnClickListener {
     private static final String TAG = "PhotoActivity";
     private static final String URL = "URL";
 
     @Bind(R.id.meizi_image)
     PinchImageView pinchImageView;
+    @Bind(R.id.btn_back)
+    ImageView mBtnBack;
+    @Bind(R.id.btn_save)
+    ImageView mBtnSave;
 
     private String mUrl;
+    private Bitmap mBitmap;
 
-    public static void startActivity(Context context, String url){
+    public static void startActivity(Context context, String url) {
         Intent intent = new Intent(context, PhotoActivity.class);
-        intent.putExtra(URL,url);
+        intent.putExtra(URL, url);
         context.startActivity(intent);
-        ((Activity)context).overridePendingTransition(R.anim.in_from_right, 0);
+        ((Activity) context).overridePendingTransition(R.anim.in_from_right, 0);
     }
 
     @Override
@@ -41,15 +53,39 @@ public class PhotoActivity extends AppSwipeBackActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
         ButterKnife.bind(this);
-
+        mBtnBack.setOnClickListener(this);
+        mBtnSave.setOnClickListener(this);
         Intent intent = getIntent();
         mUrl = intent.getStringExtra(URL);
         Log.d(TAG, "url: " + mUrl);
         Glide.with(this)
                 .load(mUrl)
+                .asBitmap()
                 .error(R.drawable.bg_cyan)
                 .placeholder(R.drawable.bg_cyan)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .into(pinchImageView);
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                        pinchImageView.setImageBitmap(resource);
+                        mBitmap = resource;
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_back:
+                Logger.d("点击back,结束Activity");
+                finish();
+                break;
+            case R.id.btn_save:
+                Logger.d("点击保存,保存图片");
+                Toast.makeText(this,"保存图片",Toast.LENGTH_LONG).show();
+                ImageSaveUtil.saveImageToGallery(this,mBitmap);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/jerey/keepgank/utils/ImageSaveUtil.java
@@ -0,0 +1,51 @@
+package com.jerey.keepgank.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+/**
+ * Created by xiamin on 3/3/17.
+ */
+
+public final class ImageSaveUtil {
+
+    public static void saveImageToGallery(Context context, Bitmap bmp) {
+        // 首先保存图片
+        File appDir = new File(Environment.getExternalStorageDirectory(), "Gank");
+        if (!appDir.exists()) {
+            appDir.mkdir();
+        }
+        String fileName = System.currentTimeMillis() + ".jpg";
+        File file = new File(appDir, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 其次把文件插入到系统图库
+        try {
+            MediaStore.Images.Media.insertImage(context.getContentResolver(),
+                    file.getAbsolutePath(), fileName, null);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        // 最后通知图库更新
+        context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + Environment.getExternalStorageDirectory() )));
+    }
+}
