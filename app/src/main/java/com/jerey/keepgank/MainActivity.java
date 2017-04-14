567ce705f7d669006d4e1553d9cc0b0144d2d99b
Xiamin <610315802@qq.com>
2017-04-14 21:38:14 +0800

    解决issues2 : 对于4.4版本以下 不做FLAG_TRANSLUCENT_STATUS设置

--- app/src/main/java/com/jerey/keepgank/MainActivity.java
+++ app/src/main/java/com/jerey/keepgank/MainActivity.java
@@ -80,7 +80,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 透明状态栏
+            getWindow().addFlags( WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // 透明导航栏
+            getWindow().addFlags( WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         mHeadViewContainer = mNavigationView.getHeaderView(0);
